47e6d3888e1c87ed7edad300f8bfef6f1ed17ac0
Adam Bennett <adam@blockchain.com>
2018-08-06 11:29:37 +0100

    feat(Homebrew): AND-1319 Move profile page to new package

--- kyc/src/main/java/com/blockchain/kycui/profile/KycProfileFragment.kt
+++ kyc/src/main/java/com/blockchain/kycui/profile/KycProfileFragment.kt
@@ -1,10 +1,11 @@
-package com.blockchain.kycui
+package com.blockchain.kycui.profile
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.blockchain.kycui.KycProgressListener
 import piuk.blockchain.androidcoreui.utils.ParentActivityDelegate
 import piuk.blockchain.androidcoreui.utils.extensions.inflate
 
@@ -21,6 +22,6 @@ class KycProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        progressListener.onProgressUpdated(10, R.string.kyc_profile_title)
+        progressListener.onProgressUpdated(20, R.string.kyc_profile_title)
     }
 }
