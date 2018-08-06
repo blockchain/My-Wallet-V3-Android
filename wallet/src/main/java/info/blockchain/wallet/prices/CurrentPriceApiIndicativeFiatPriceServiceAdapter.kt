6de0bdd9c692b45edc0ee8a748d4ee4922746214
Alan Evans <alan@blockchain.com>
2018-08-06 10:09:59 -0300

    feat(HomeBrew): AND-1321 Indicative Crypto to Fiat price stream

---
+++ wallet/src/main/java/info/blockchain/wallet/prices/CurrentPriceApiIndicativeFiatPriceServiceAdapter.kt
@@ -0,0 +1,33 @@
+package info.blockchain.wallet.prices
+
+import info.blockchain.balance.CryptoCurrency
+import info.blockchain.balance.ExchangeRate
+import io.reactivex.Observable
+import io.reactivex.Single
+import java.util.concurrent.TimeUnit
+
+internal fun CurrentPriceApi.toIndicativeFiatPriceService(): IndicativeFiatPriceService {
+    return CurrentPriceApiIndicativeFiatPriceServiceAdapter(this)
+}
+
+private class CurrentPriceApiIndicativeFiatPriceServiceAdapter(
+    private val currentPriceApi: CurrentPriceApi
+) : IndicativeFiatPriceService {
+
+    override fun indicativeRateStream(from: CryptoCurrency, toFiat: String): Observable<ExchangeRate.CryptoToFiat> =
+        repeat {
+            currentPriceApi.currentPrice(from, toFiat)
+        }.retry()
+            .map {
+                ExchangeRate.CryptoToFiat(
+                    from,
+                    toFiat,
+                    it
+                )
+            }
+}
+
+private fun <T> repeat(function: () -> Single<T>): Observable<T> =
+    Observable.fromCallable(function)
+        .flatMap { it.toObservable() }
+        .repeatWhen { o -> o.concatMap { _ -> Observable.timer(1, TimeUnit.SECONDS) } }
