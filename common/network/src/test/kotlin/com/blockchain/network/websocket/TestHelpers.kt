d72199591c5057bd43287e707763b27b52f1a75b
Alan Evans <alan@blockchain.com>
2018-08-13 11:01:09 -0300

    feat(WebSocket): AND-1325 Spotted a junit.framework import

--- common/network/src/test/kotlin/com/blockchain/network/websocket/TestHelpers.kt
+++ common/network/src/test/kotlin/com/blockchain/network/websocket/TestHelpers.kt
@@ -3,7 +3,7 @@ package com.blockchain.network.websocket
 import com.blockchain.testutils.after
 import com.blockchain.testutils.before
 import io.fabric8.mockwebserver.DefaultMockServer
-import junit.framework.Assert
+import org.junit.Assert.assertTrue
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 
@@ -16,7 +16,7 @@ class MessageWaiter(numberOfMessages: Int) : WebSocket.Listener<Any> by WebSocke
     }
 
     fun waitForAllMessages() {
-        Assert.assertTrue("Never got all the messages", messageLatch.await(1, TimeUnit.SECONDS))
+        assertTrue("Never got all the messages", messageLatch.await(1, TimeUnit.SECONDS))
     }
 }
 
