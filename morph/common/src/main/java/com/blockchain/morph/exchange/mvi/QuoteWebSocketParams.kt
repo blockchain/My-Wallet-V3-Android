0d7b0e8ee429ec9722949a0732e341b53cac99ac
Alan Evans <alan@blockchain.com>
2018-08-17 11:10:27 -0300

    feat(HomeBrew): AND-1336 ExchangeQuoteRequest to QuoteWebSocketParams mapping

---
+++ morph/common/src/main/java/com/blockchain/morph/exchange/mvi/QuoteWebSocketParams.kt
@@ -0,0 +1,48 @@
+package com.blockchain.morph.exchange.mvi
+
+import com.blockchain.morph.quote.ExchangeQuoteRequest
+import info.blockchain.balance.format
+import io.reactivex.Observable
+import java.util.Locale
+
+data class QuoteWebSocketParams(
+    val pair: String,
+    val volume: String,
+    val volumeCurrency: String,
+    val isBase: Boolean?
+)
+
+fun Observable<ExchangeQuoteRequest>.mapToSocketParameters(): Observable<QuoteWebSocketParams> =
+    map(ExchangeQuoteRequest::mapToSocketParameters)
+
+internal fun ExchangeQuoteRequest.mapToSocketParameters() =
+    when (this) {
+        is ExchangeQuoteRequest.Selling ->
+            QuoteWebSocketParams(
+                pair = pair.pairCodeUpper,
+                volume = offering.format(),
+                volumeCurrency = offering.currency.symbol,
+                isBase = null
+            )
+        is ExchangeQuoteRequest.SellingFiatLinked ->
+            QuoteWebSocketParams(
+                pair = pair.pairCodeUpper,
+                volume = offeringFiatValue.toStringWithoutSymbol(Locale.US),
+                volumeCurrency = offeringFiatValue.currencyCode,
+                isBase = true
+            )
+        is ExchangeQuoteRequest.Buying ->
+            QuoteWebSocketParams(
+                pair = pair.inverse().pairCodeUpper,
+                volume = wanted.format(),
+                volumeCurrency = wanted.currency.symbol,
+                isBase = null
+            )
+        is ExchangeQuoteRequest.BuyingFiatLinked ->
+            QuoteWebSocketParams(
+                pair = pair.pairCodeUpper,
+                volume = wantedFiatValue.toStringWithoutSymbol(Locale.US),
+                volumeCurrency = wantedFiatValue.currencyCode,
+                isBase = false
+            )
+    }
---
+++ morph/common/src/test/java/com/blockchain/morph/exchange/mvi/QuoteWebSocketParamsTest.kt
@@ -0,0 +1,88 @@
+package com.blockchain.morph.exchange.mvi
+
+import com.blockchain.morph.quote.ExchangeQuoteRequest
+import info.blockchain.balance.CryptoCurrency
+import io.reactivex.Observable
+import org.amshove.kluent.`should equal`
+import org.junit.Test
+
+class QuoteWebSocketParamsTest {
+
+    @Test
+    fun `Selling crypto`() {
+        ExchangeQuoteRequest.Selling(
+            offering = 2.0.bitcoin(),
+            wanted = CryptoCurrency.ETHER
+        ).mapToSocketParameters() `should equal`
+            QuoteWebSocketParams(
+                pair = "BTC-ETH",
+                volume = "2.0",
+                volumeCurrency = "BTC",
+                isBase = null
+            )
+    }
+
+    @Test
+    fun `Buying crypto`() {
+        ExchangeQuoteRequest.Buying(
+            offering = CryptoCurrency.BCH,
+            wanted = 1.0.bitcoin()
+        ).mapToSocketParameters() `should equal`
+            QuoteWebSocketParams(
+                pair = "BTC-BCH",
+                volume = "1.0",
+                volumeCurrency = "BTC",
+                isBase = null
+            )
+    }
+
+    @Test
+    fun `Selling crypto - fiat linked`() {
+        ExchangeQuoteRequest.SellingFiatLinked(
+            offering = CryptoCurrency.BTC,
+            wanted = CryptoCurrency.ETHER,
+            offeringFiatValue = 12.34.usd()
+        ).mapToSocketParameters() `should equal`
+            QuoteWebSocketParams(
+                pair = "BTC-ETH",
+                volume = "12.34",
+                volumeCurrency = "USD",
+                isBase = true
+            )
+    }
+
+    @Test
+    fun `Buying crypto - fiat linked`() {
+        ExchangeQuoteRequest.BuyingFiatLinked(
+            offering = CryptoCurrency.ETHER,
+            wanted = CryptoCurrency.BCH,
+            wantedFiatValue = 45.67.usd()
+        ).mapToSocketParameters() `should equal`
+            QuoteWebSocketParams(
+                pair = "ETH-BCH",
+                volume = "45.67",
+                volumeCurrency = "USD",
+                isBase = false
+            )
+    }
+
+    @Test
+    fun `map several`() {
+        Observable.just<ExchangeQuoteRequest>(
+            ExchangeQuoteRequest.Selling(
+                offering = 2.0.bitcoin(),
+                wanted = CryptoCurrency.ETHER
+            ),
+            ExchangeQuoteRequest.Buying(
+                wanted = 4.0.ether(),
+                offering = CryptoCurrency.BCH
+            )
+        ).mapToSocketParameters()
+            .test()
+            .values() `should equal`
+            listOf(
+                QuoteWebSocketParams("BTC-ETH", "2.0", "BTC", null),
+                QuoteWebSocketParams("ETH-BCH", "4.0", "ETH", null)
+            )
+    }
+}
