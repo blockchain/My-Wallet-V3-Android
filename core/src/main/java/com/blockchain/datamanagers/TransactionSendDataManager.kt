9cfd3f0fc6f0be102c14a04aed4b7a5cd9ea7554
Adam Bennett <adam@blockchain.com>
2018-09-17 11:39:50 +0100

    chore(Transactions): AND-1372 Create single datamanager for handling all cryptocurrency transactions

---
+++ core/src/main/java/com/blockchain/datamanagers/TransactionSendDataManager.kt
@@ -0,0 +1,204 @@
+package com.blockchain.datamanagers
+
+import com.blockchain.serialization.JsonSerializableAccount
+import info.blockchain.api.data.UnspentOutputs
+import info.blockchain.balance.CryptoCurrency
+import info.blockchain.balance.CryptoValue
+import info.blockchain.wallet.api.data.FeeOptions
+import info.blockchain.wallet.coin.GenericMetadataAccount
+import info.blockchain.wallet.ethereum.EthereumAccount
+import info.blockchain.wallet.payload.data.Account
+import info.blockchain.wallet.payment.SpendableUnspentOutputs
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
+import org.bitcoinj.core.ECKey
+import org.web3j.utils.Convert
+import piuk.blockchain.androidcore.data.bitcoincash.BchDataManager
+import piuk.blockchain.androidcore.data.ethereum.EthDataManager
+import piuk.blockchain.androidcore.data.ethereum.EthereumAccountWrapper
+import piuk.blockchain.androidcore.data.payload.PayloadDataManager
+import piuk.blockchain.androidcore.data.payments.SendDataManager
+import java.math.BigInteger
+
+class TransactionSendDataManager(
+    private val payloadDataManager: PayloadDataManager,
+    private val ethDataManager: EthDataManager,
+    private val bchDataManager: BchDataManager,
+    private val sendDataManager: SendDataManager,
+    private val ethereumAccountWrapper: EthereumAccountWrapper
+) {
+
+    fun executeTransaction(
+        amount: CryptoValue,
+        destination: String,
+        account: JsonSerializableAccount,
+        fees: FeeOptions,
+        feeType: FeeType = FeeType.Regular
+    ): Single<String> = when (amount.currency) {
+        CryptoCurrency.BTC -> sendBtcTransaction(
+            amount,
+            destination,
+            account as Account,
+            fees.feeForType(feeType)
+        )
+        CryptoCurrency.ETHER -> sendEthTransaction(
+            amount,
+            destination,
+            account as EthereumAccount,
+            fees
+        )
+        CryptoCurrency.BCH -> sendBchTransaction(
+            amount,
+            destination,
+            account as GenericMetadataAccount,
+            fees.feeForType(feeType)
+        )
+    }
+
+    private fun sendBtcTransaction(
+        amount: CryptoValue,
+        destination: String,
+        account: Account,
+        feePerKb: BigInteger
+    ): Single<String> = getSpendableCoins(account.xpub, amount, feePerKb)
+        .flatMap { spendable ->
+            getSigningKeys(account, spendable)
+                .flatMap { signingKeys ->
+                    account.getChangeAddress()
+                        .flatMap {
+                            submitBitcoinStylePayment(
+                                amount,
+                                spendable,
+                                signingKeys,
+                                destination,
+                                it,
+                                feePerKb
+                            )
+                        }
+                }
+        }
+
+    private fun sendBchTransaction(
+        amount: CryptoValue,
+        destination: String,
+        account: GenericMetadataAccount,
+        feePerKb: BigInteger
+    ): Single<String> = getSpendableCoins(account.xpub, amount, feePerKb)
+        .flatMap { spendable ->
+            getSigningKeys(account.getHdAccount(), spendable)
+                .flatMap { signingKeys ->
+                    account.getChangeAddress()
+                        .flatMap {
+                            submitBitcoinStylePayment(
+                                amount,
+                                spendable,
+                                signingKeys,
+                                destination,
+                                it,
+                                feePerKb
+                            )
+                        }
+                }
+        }
+
+    private fun sendEthTransaction(
+        amount: CryptoValue,
+        destination: String,
+        account: EthereumAccount,
+        fees: FeeOptions
+    ): Single<String> = ethDataManager.fetchEthAddress()
+        .map {
+            ethDataManager.createEthTransaction(
+                nonce = ethDataManager.getEthResponseModel()!!.getNonce(),
+                to = destination,
+                gasPrice = fees.regularFee.gasPriceToWei(),
+                gasLimit = fees.gasLimit.toBigInteger(),
+                weiValue = amount.amount
+            )
+        }
+        .map {
+            account.signTransaction(
+                it,
+                ethereumAccountWrapper.deriveECKey(payloadDataManager.masterKey, 0)
+            )
+        }
+        .flatMap { ethDataManager.pushEthTx(it) }
+        .flatMap { ethDataManager.setLastTxHashObservable(it, System.currentTimeMillis()) }
+        .subscribeOn(Schedulers.io())
+        .singleOrError()
+
+    private fun getSpendableCoins(
+        address: String,
+        amount: CryptoValue,
+        feePerKb: BigInteger
+    ): Single<SpendableUnspentOutputs> = getUnspentOutputs(address, amount)
+        .subscribeOn(Schedulers.io())
+        .map { sendDataManager.getSpendableCoins(it, amount.amount, feePerKb) }
+
+    private fun getUnspentOutputs(address: String, amount: CryptoValue): Single<UnspentOutputs> =
+        when (amount.currency) {
+            CryptoCurrency.BTC -> sendDataManager.getUnspentOutputs(address)
+            CryptoCurrency.BCH -> sendDataManager.getUnspentBchOutputs(address)
+            CryptoCurrency.ETHER -> throw IllegalArgumentException("Ether does not have unspent outputs")
+        }.subscribeOn(Schedulers.io())
+            .singleOrError()
+
+    private fun submitBitcoinStylePayment(
+        amount: CryptoValue,
+        unspent: SpendableUnspentOutputs,
+        signingKeys: List<ECKey>,
+        depositAddress: String,
+        changeAddress: String,
+        feePerKb: BigInteger
+    ): Single<String> = when (amount.currency) {
+        CryptoCurrency.BTC -> sendDataManager.submitBtcPayment(
+            unspent,
+            signingKeys,
+            depositAddress,
+            changeAddress,
+            feePerKb,
+            amount.amount
+        )
+        CryptoCurrency.BCH -> sendDataManager.submitBchPayment(
+            unspent,
+            signingKeys,
+            depositAddress,
+            changeAddress,
+            feePerKb,
+            amount.amount
+        )
+        CryptoCurrency.ETHER -> throw IllegalArgumentException("Ether not supported by this method")
+    }.subscribeOn(Schedulers.io())
+        .singleOrError()
+
+    private fun getSigningKeys(
+        account: Account,
+        spendable: SpendableUnspentOutputs
+    ): Single<List<ECKey>> =
+        Single.just(payloadDataManager.getHDKeysForSigning(account, spendable))
+
+    private fun Account.getChangeAddress(): Single<String> =
+        payloadDataManager.getNextChangeAddress(this).singleOrError()
+
+    private fun GenericMetadataAccount.getChangeAddress(): Single<String> {
+        val position = bchDataManager.getActiveAccounts()
+            .indexOfFirst { it.xpub == this.xpub }
+        return bchDataManager.getNextChangeAddress(position).singleOrError()
+    }
+
+    private fun FeeOptions.feeForType(feeType: FeeType): BigInteger = when (feeType) {
+        FeeType.Regular -> regularFee
+        FeeType.Priority -> priorityFee
+    }.toBigInteger()
+
+    private fun GenericMetadataAccount.getHdAccount(): Account =
+        payloadDataManager.getAccountForXPub(this.xpub)
+}
+
+internal fun Long.gasPriceToWei(): BigInteger =
+    Convert.toWei(this.toBigDecimal(), Convert.Unit.GWEI).toBigInteger()
+
+sealed class FeeType {
+    object Regular : FeeType()
+    object Priority : FeeType()
+}
---
+++ core/src/test/java/com/blockchain/datamanagers/TransactionSendDataManagerTest.kt
@@ -0,0 +1,319 @@
+package com.blockchain.datamanagers
+
+import com.blockchain.android.testutils.rxInit
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.eq
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
+import info.blockchain.api.data.UnspentOutputs
+import info.blockchain.balance.CryptoCurrency
+import info.blockchain.balance.CryptoValue
+import info.blockchain.wallet.api.data.FeeOptions
+import info.blockchain.wallet.coin.GenericMetadataAccount
+import info.blockchain.wallet.ethereum.EthereumAccount
+import info.blockchain.wallet.payload.data.Account
+import info.blockchain.wallet.payment.SpendableUnspentOutputs
+import io.reactivex.Observable
+import org.amshove.kluent.mock
+import org.bitcoinj.core.ECKey
+import org.bitcoinj.crypto.DeterministicKey
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.web3j.crypto.RawTransaction
+import piuk.blockchain.androidcore.data.bitcoincash.BchDataManager
+import piuk.blockchain.androidcore.data.ethereum.EthDataManager
+import piuk.blockchain.androidcore.data.ethereum.EthereumAccountWrapper
+import piuk.blockchain.androidcore.data.ethereum.models.CombinedEthModel
+import piuk.blockchain.androidcore.data.payload.PayloadDataManager
+import piuk.blockchain.androidcore.data.payments.SendDataManager
+import java.math.BigInteger
+
+class TransactionSendDataManagerTest {
+
+    private lateinit var subject: TransactionSendDataManager
+    private val payloadDataManager: PayloadDataManager = mock()
+    private val ethDataManager: EthDataManager = mock()
+    private val bchDataManager: BchDataManager = mock()
+    private val sendDataManager: SendDataManager = mock()
+    private val ethereumAccountWrapper: EthereumAccountWrapper = mock()
+
+    @Suppress("unused")
+    @get:Rule
+    val initSchedulers = rxInit {
+        mainTrampoline()
+        ioTrampoline()
+    }
+
+    @Before
+    fun setUp() {
+        subject = TransactionSendDataManager(
+            payloadDataManager,
+            ethDataManager,
+            bchDataManager,
+            sendDataManager,
+            ethereumAccountWrapper
+        )
+    }
+
+    @Test
+    fun `execute bitcoin transaction should attempt to send bitcoin`() {
+        // Arrange
+        val amount = CryptoValue(CryptoCurrency.BTC, BigInteger.TEN)
+        val destination = "DESTINATION"
+        val account = Account().apply { xpub = "XPUB" }
+        whenever(sendDataManager.getUnspentOutputs("XPUB"))
+            .thenReturn(Observable.empty())
+        // Act
+        subject.executeTransaction(amount, destination, account, feeOptions)
+            .test()
+        // Assert
+        verify(sendDataManager).getUnspentOutputs("XPUB")
+    }
+
+    @Test
+    fun `execute bitcoin cash transaction should attempt to send bitcoin cash`() {
+        // Arrange
+        val amount = CryptoValue(CryptoCurrency.BCH, BigInteger.TEN)
+        val destination = "DESTINATION"
+        val account = GenericMetadataAccount().apply { xpub = "XPUB" }
+        whenever(sendDataManager.getUnspentBchOutputs("XPUB"))
+            .thenReturn(Observable.empty())
+        // Act
+        subject.executeTransaction(amount, destination, account, feeOptions)
+            .test()
+        // Assert
+        verify(sendDataManager).getUnspentBchOutputs("XPUB")
+    }
+
+    @Test
+    fun `execute ethereum transaction should attempt to send ethereum`() {
+        // Arrange
+        val amount = CryptoValue(CryptoCurrency.ETHER, BigInteger.TEN)
+        val destination = "DESTINATION"
+        val account = EthereumAccount()
+        whenever(ethDataManager.fetchEthAddress())
+            .thenReturn(Observable.empty())
+        // Act
+        subject.executeTransaction(amount, destination, account, feeOptions)
+            .test()
+        // Assert
+        verify(ethDataManager).fetchEthAddress()
+    }
+
+    @Test
+    fun `execute bitcoin transaction should set regular fee by default`() {
+        // Arrange
+        val amount = CryptoValue(CryptoCurrency.BTC, BigInteger.TEN)
+        val destination = "DESTINATION"
+        val account = Account().apply { xpub = "XPUB" }
+        val unspentOutputs = UnspentOutputs()
+        whenever(sendDataManager.getUnspentOutputs("XPUB"))
+            .thenReturn(Observable.just(unspentOutputs))
+        whenever(sendDataManager.getSpendableCoins(any(), any(), any()))
+            .thenReturn(SpendableUnspentOutputs())
+        // Act
+        subject.executeTransaction(amount, destination, account, feeOptions)
+            .test()
+        // Assert
+        verify(sendDataManager).getUnspentOutputs("XPUB")
+        verify(sendDataManager).getSpendableCoins(
+            unspentOutputs,
+            amount.amount,
+            feeOptions.regularFee.toBigInteger()
+        )
+    }
+
+    @Test
+    fun `execute bitcoin transaction with high priority fee`() {
+        // Arrange
+        val amount = CryptoValue(CryptoCurrency.BTC, BigInteger.TEN)
+        val destination = "DESTINATION"
+        val account = Account().apply { xpub = "XPUB" }
+        val unspentOutputs = UnspentOutputs()
+        whenever(sendDataManager.getUnspentOutputs("XPUB"))
+            .thenReturn(Observable.just(unspentOutputs))
+        whenever(sendDataManager.getSpendableCoins(any(), any(), any()))
+            .thenReturn(SpendableUnspentOutputs())
+        // Act
+        subject.executeTransaction(amount, destination, account, feeOptions, FeeType.Priority)
+            .test()
+        // Assert
+        verify(sendDataManager).getUnspentOutputs("XPUB")
+        verify(sendDataManager).getSpendableCoins(
+            unspentOutputs,
+            BigInteger.TEN,
+            feeOptions.priorityFee.toBigInteger()
+        )
+    }
+
+    @Test
+    fun `execute bitcoin transaction verify entire flow`() {
+        // Arrange
+        val amount = CryptoValue(CryptoCurrency.BTC, BigInteger.TEN)
+        val destination = "DESTINATION"
+        val change = "CHANGE"
+        val account = Account().apply { xpub = "XPUB" }
+        val unspentOutputs = UnspentOutputs()
+        whenever(sendDataManager.getUnspentOutputs("XPUB"))
+            .thenReturn(Observable.just(unspentOutputs))
+        val spendable = SpendableUnspentOutputs()
+        whenever(sendDataManager.getSpendableCoins(any(), any(), any()))
+            .thenReturn(spendable)
+        val ecKey = ECKey()
+        whenever(payloadDataManager.getHDKeysForSigning(account, spendable))
+            .thenReturn(listOf(ecKey))
+        whenever(payloadDataManager.getNextChangeAddress(account))
+            .thenReturn(Observable.just(change))
+        val txHash = "TX_ HASH"
+        whenever(
+            sendDataManager.submitBtcPayment(
+                spendable,
+                listOf(ecKey),
+                destination,
+                change,
+                feeOptions.regularFee.toBigInteger(),
+                amount.amount
+            )
+        ).thenReturn(Observable.just(txHash))
+        // Act
+        val testObserver =
+            subject.executeTransaction(amount, destination, account, feeOptions)
+                .test()
+        // Assert
+        testObserver.assertComplete()
+        testObserver.assertValue(txHash)
+        verify(sendDataManager).getUnspentOutputs("XPUB")
+        verify(sendDataManager).getSpendableCoins(
+            unspentOutputs,
+            BigInteger.TEN,
+            feeOptions.regularFee.toBigInteger()
+        )
+        verify(sendDataManager).submitBtcPayment(
+            spendable,
+            listOf(ecKey),
+            destination,
+            change,
+            feeOptions.regularFee.toBigInteger(),
+            amount.amount
+        )
+    }
+
+    @Test
+    fun `execute bitcoin cash transaction verify entire flow`() {
+        // Arrange
+        val amount = CryptoValue(CryptoCurrency.BCH, BigInteger.TEN)
+        val destination = "DESTINATION"
+        val change = "CHANGE"
+        val bchAccount = GenericMetadataAccount().apply { xpub = "XPUB" }
+        val account = Account().apply { xpub = "XPUB" }
+        val unspentOutputs = UnspentOutputs()
+        whenever(sendDataManager.getUnspentBchOutputs("XPUB"))
+            .thenReturn(Observable.just(unspentOutputs))
+        val spendable = SpendableUnspentOutputs()
+        whenever(sendDataManager.getSpendableCoins(any(), any(), any()))
+            .thenReturn(spendable)
+        whenever(bchDataManager.getActiveAccounts()).thenReturn(listOf(bchAccount))
+        whenever(payloadDataManager.getAccountForXPub("XPUB"))
+            .thenReturn(account)
+        val ecKey = ECKey()
+        whenever(payloadDataManager.getHDKeysForSigning(account, spendable))
+            .thenReturn(listOf(ecKey))
+        whenever(bchDataManager.getNextChangeAddress(0))
+            .thenReturn(Observable.just(change))
+        val txHash = "TX_ HASH"
+        whenever(
+            sendDataManager.submitBchPayment(
+                spendable,
+                listOf(ecKey),
+                destination,
+                change,
+                feeOptions.regularFee.toBigInteger(),
+                amount.amount
+            )
+        ).thenReturn(Observable.just(txHash))
+        // Act
+        val testObserver =
+            subject.executeTransaction(amount, destination, bchAccount, feeOptions)
+                .test()
+        // Assert
+        testObserver.assertComplete()
+        testObserver.assertValue(txHash)
+        verify(sendDataManager).getUnspentBchOutputs("XPUB")
+        verify(sendDataManager).getSpendableCoins(
+            unspentOutputs,
+            BigInteger.TEN,
+            feeOptions.regularFee.toBigInteger()
+        )
+        verify(sendDataManager).submitBchPayment(
+            spendable,
+            listOf(ecKey),
+            destination,
+            change,
+            feeOptions.regularFee.toBigInteger(),
+            amount.amount
+        )
+    }
+
+    @Test
+    fun `execute ethereum transaction verify entire flow`() {
+        // Arrange
+        val amount = CryptoValue(CryptoCurrency.ETHER, BigInteger.TEN)
+        val destination = "DESTINATION"
+        val account: EthereumAccount = mock()
+        val combinedEthModel: CombinedEthModel = mock()
+        whenever(ethDataManager.fetchEthAddress())
+            .thenReturn(Observable.just(combinedEthModel))
+        whenever(ethDataManager.getEthResponseModel())
+            .thenReturn(combinedEthModel)
+        whenever(combinedEthModel.getNonce())
+            .thenReturn(BigInteger.ONE)
+        val rawTransaction: RawTransaction = mock()
+        whenever(
+            ethDataManager.createEthTransaction(
+                BigInteger.ONE,
+                destination,
+                feeOptions.regularFee.gasPriceToWei(),
+                feeOptions.gasLimit.toBigInteger(),
+                amount.amount
+            )
+        ).thenReturn(rawTransaction)
+        val deterministicKey: DeterministicKey = mock()
+        whenever(payloadDataManager.masterKey)
+            .thenReturn(deterministicKey)
+        val ecKey = ECKey()
+        whenever(ethereumAccountWrapper.deriveECKey(deterministicKey, 0))
+            .thenReturn(ecKey)
+        val signedTx = ByteArray(0)
+        whenever(account.signTransaction(rawTransaction, ecKey))
+            .thenReturn(signedTx)
+        val txHash = "TX_HASH"
+        whenever(ethDataManager.pushEthTx(signedTx))
+            .thenReturn(Observable.just(txHash))
+        whenever(ethDataManager.setLastTxHashObservable(eq(txHash), any()))
+            .thenReturn(Observable.just(txHash))
+        // Act
+        val testObserver = subject.executeTransaction(amount, destination, account, feeOptions)
+            .test()
+        // Assert
+        testObserver.assertComplete()
+        testObserver.assertValue(txHash)
+        verify(ethDataManager).fetchEthAddress()
+        verify(ethDataManager).getEthResponseModel()
+        verify(ethDataManager).createEthTransaction(
+            BigInteger.ONE,
+            destination,
+            feeOptions.regularFee.gasPriceToWei(),
+            feeOptions.gasLimit.toBigInteger(),
+            amount.amount
+        )
+        verify(ethDataManager).pushEthTx(signedTx)
+        verify(ethDataManager).setLastTxHashObservable(eq(txHash), any())
+    }
+
+    private val feeOptions = FeeOptions().apply {
+        priorityFee = 100L
+        regularFee = 10L
+        gasLimit = 21000L
+    }
+}
