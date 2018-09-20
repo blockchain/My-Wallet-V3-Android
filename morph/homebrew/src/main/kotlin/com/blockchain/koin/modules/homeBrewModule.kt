a829075eac380c8fe76da0eef4625a211de3e2f2
Alan Evans <alan@blockchain.com>
2018-09-20 11:29:16 +0100

    feat(Homebrew): AND-1352 Break dependency from nabu to morph

--- morph/homebrew/src/main/kotlin/com/blockchain/koin/modules/homeBrewModule.kt
+++ morph/homebrew/src/main/kotlin/com/blockchain/koin/modules/homeBrewModule.kt
@@ -2,6 +2,8 @@ package com.blockchain.koin.modules
 
 import com.blockchain.morph.QuoteWebSocketServiceFactory
 import com.blockchain.morph.exchange.service.QuoteServiceFactory
+import com.blockchain.morph.trade.MorphTradeDataManager
+import com.blockchain.nabu.datamanagers.NabuDataManagerAdapter
 import org.koin.dsl.module.applicationContext
 
 val homeBrewModule = applicationContext {
@@ -11,5 +13,7 @@ val homeBrewModule = applicationContext {
         factory {
             QuoteWebSocketServiceFactory(get("nabu"), get(), get(), get()) as QuoteServiceFactory
         }
+
+        factory { NabuDataManagerAdapter(get()) as MorphTradeDataManager }
     }
 }
