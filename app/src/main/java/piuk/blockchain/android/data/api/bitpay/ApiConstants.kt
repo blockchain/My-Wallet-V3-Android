044a4a9414aeb0993156dae6f5355de55cb6ce0c
alex_soukhov <alex.soukhov@gmail.com>
2021-07-27 10:58:45 +0300

    ppv2 in progress

--- app/src/main/java/piuk/blockchain/android/data/api/bitpay/ApiConstants.kt
+++ app/src/main/java/piuk/blockchain/android/data/api/bitpay/ApiConstants.kt
@@ -11,3 +11,15 @@ const val BITPAY_LIVE_BASE: String = "https://bitpay.com/"
  */
 
 const val PATH_BITPAY_INVOICE: String = "i"
+
+/**
+ * Base URLs for Lunu
+ */
+
+const val LUNU_LIVE_BASE: String = "https://api.rc.lunu.io/"
+
+/**
+ * Paths for BitPay
+ */
+
+const val PATH_LUNU_INVOICE: String = "v1/payment/ppv2"
